1c6643701b34435c35dc844478856c86d28b38ef
CleberGraciano <cleber_batista2010@hotmail.com>
2018-02-23 14:34:01 -0300

    Subindo War

--- src/main/java/br/pro/delfino/drogaria/dao/EstadoDao.java
+++
@@ -1,8 +0,0 @@
-package br.pro.delfino.drogaria.dao;
-
-import br.pro.delfino.drogaria.domain.Estado;
-
-public class EstadoDao extends GenericDao<Estado> {
-	
-
-}
--- src/main/java/br/pro/delfino/drogaria/dao/ItemVendaDao.java
+++
@@ -1,7 +0,0 @@
-package br.pro.delfino.drogaria.dao;
-
-import br.pro.delfino.drogaria.domain.ItemVenda;
-
-public class ItemVendaDao extends GenericDao<ItemVenda> {
-
-}
